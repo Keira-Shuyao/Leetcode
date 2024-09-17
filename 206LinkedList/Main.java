59c7aa9a90d8ecc9493626e37d68411604a1019d
Keira <nicolelelele1227@gmail.com>
2024-09-17 16:25:16 -0700

    9.16 LinkedList reverse

---
+++ 206LinkedList/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        ListNode newHead = reverseList(next);
+        next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode node3 = new ListNode(3);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+
+        System.out.println("Original List:");
+        printList(node1);
+
+        ListNode reversed = reverseList(node1);
+
+        System.out.println("Reversed List:");
+        printList(reversed);
+    }
+}
