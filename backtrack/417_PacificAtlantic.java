796c169f6b4b5b4772ee15e58fba830d7686cedd
KeiraMa <keira.sy.ma@outlook.com>
2024-12-24 22:45:37 -0800

    从边界出发的DFS，答案在重叠的地方

---
+++ backtrack/417_PacificAtlantic.java
@@ -0,0 +1,73 @@
+class Solution {
+    //这题和130实现方式一样，都是从边界出发，但是最后一步不一样，这里是找重叠部分，130是把格子替换
+    private int m, n;
+private int[][] matrix;
+private int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+public List<List<Integer>> pacificAtlantic(int[][] matrix) {
+    List<List<Integer>> ret = new ArrayList<>();
+    if (matrix == null || matrix.length == 0) {
+        return ret;
+    }
+
+    m = matrix.length;
+    n = matrix[0].length;
+    this.matrix = matrix;
+    boolean[][] canReachP = new boolean[m][n];
+    boolean[][] canReachA = new boolean[m][n];
+
+    for (int i = 0; i < m; i++) {
+        dfs(i, 0, canReachP);
+        dfs(i, n - 1, canReachA);
+    }
+    for (int i = 0; i < n; i++) {
+        dfs(0, i, canReachP);
+        dfs(m - 1, i, canReachA);
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (canReachP[i][j] && canReachA[i][j]) {
+                ret.add(Arrays.asList(i, j));
+            }
+        }
+    }
+
+    return ret;
+}
+
+private void dfs(int r, int c, boolean[][] canReach) {
+    if (canReach[r][c]) {
+        return;
+    }
+    canReach[r][c] = true;
+    for (int[] d : direction) {
+        int nextR = d[0] + r;
+        int nextC = d[1] + c;
+        if (nextR < 0 || nextR >= m || nextC < 0 || nextC >= n
+                || matrix[r][c] > matrix[nextR][nextC]) {
+
+            continue;
+        }
+        dfs(nextR, nextC, canReach);
+    }
+}
+}
+
+/**
+The problem is solved backwards:
+
+Instead of checking if water can flow to oceans
+We check if we can reach a cell from the ocean
+This makes it easier as we're going up in height
+分别从两个海的边界出发，只有当前格子《下一个格子的时候，才会继续，一直找到最大的格子，标记完成。最后重叠部分的格子就是需要返回的。
+
+The DFS conditions ensure:
+
+We stay within bounds
+We don't revisit cells
+We only move to equal or higher heights
+
+
+
+ */
